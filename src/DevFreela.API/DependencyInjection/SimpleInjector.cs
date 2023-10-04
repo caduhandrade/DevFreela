e5de12dde8f5da634a7acab31d378e4c70de5195
Cadu Andrade <dev.andradecarlos@gmail.com>
2023-10-03 22:12:33 -0300

    adicionado injetor de dependencias

---
+++ src/DevFreela.API/DependencyInjection/SimpleInjector.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.API.Models;
+using DevFreela.Application.Services.Implementations;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+
+namespace DevFreela.API.DependencyInjection
+{
+    public class SimpleInjector
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            services.AddSingleton<DevFreelaDbContext>();
+
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ISkillService, SkillService>();
+            services.AddScoped<ExampleClass>(e => new ExampleClass { Name = "Initial Stage" });
+        }
+    }
+}
