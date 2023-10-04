f72ced7ceda4dde6f108b3d90852b4b783c216f7
Cadu Andrade <dev.andradecarlos@gmail.com>
2023-10-03 22:28:04 -0300

    configurado tabela de projetos.

--- src/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ src/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -15,7 +15,31 @@ namespace DevFreela.Infrastructure.Persistence
         public DbSet<Project> Projects { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Skill> Skills { get; set; }
-        public DbSet<Skill> UserSkills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
         public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Project>()
+                .HasOne(x => x.Freelancer)
+                .WithMany(x => x.FreelanceProjects)
+                .HasForeignKey(x => x.IdFreelancer)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Skill>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<UserSkill>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<ProjectComment>()
+                .HasKey(x => x.Id); 
+        }
     }
 }
